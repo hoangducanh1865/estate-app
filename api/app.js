99c9e2da0a88f127ac55edea014b1a47ea1e3c6b
hoangducanh18651 <hoangducanh.1865.1@gmail.com>
2025-05-02 21:00:40 +0700

    Express.js React JWT Cookie Auth Tutorial + Update file required_commands.txt

--- api/app.js
+++ api/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
